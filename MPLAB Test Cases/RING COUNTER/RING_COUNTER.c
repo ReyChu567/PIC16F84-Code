cebd43d796927348189e3a0119001048557db59a
ReyChu567 <152696012+ReyChu567@users.noreply.github.com>
2025-04-21 09:08:36 +0800

    Add files via upload

---
+++ MPLAB Test Cases/RING COUNTER/RING_COUNTER.c
@@ -0,0 +1,16 @@
+	// Ring Counter for PIC16F84
+#include <xc.h>
+
+void main() {
+    TRISA = 0x00;               // Set PORTA as output
+    PORTA = 0x00;               // Initialize PORTA (00000000)
+
+    while (1) {
+		PORTA = PORTA << 1;		// Shift to the left 
+
+		// If the last LED is reached, reset to the first LED
+        if (PORTA == 0x00) {
+            PORTA = 0x01;       // Restart the ring counter
+        }
+    }
+}
